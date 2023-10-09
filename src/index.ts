c947ec60e9d7fb5c1133463ea2211f58a715f63b
Andrea <abrighi@bsdsoftware.it>
2023-10-09 17:49:20 +0200

    feat: add html render

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,53 @@
 import 'graphql-import-node';
 import 'graphql-import-node';
 import fastify from "fastify";
-import { getGraphQLParameters, processRequest, Request, sendResult } from "graphql-helix";
+import { getGraphQLParameters, processRequest, renderGraphiQL, Request, sendResult, shouldRenderGraphiQL } from "graphql-helix";
 import { schema } from "./schema/schema";
 
 async function main() {
   const server = fastify();
 
   server.route({
-    method: "POST",
-    url: "/graphql",
-    handler: async (req, reply) => {
-      const request: Request = {
-        headers: req.headers,
-        method: req.method,
-        query: req.query,
-        body: req.body,
-      };
+      method: ["POST", "GET"],
+      url: "/graphql",
+      handler: async (req, reply) => {
+        const request: Request = {
+          headers: req.headers,
+          method: req.method,
+          query: req.query,
+          body: req.body,
+        };
 
-      const { operationName, query, variables } = getGraphQLParameters(request);
-
-      const result = await processRequest({
-        request,
-        schema,
-        operationName,
-        query,
-        variables,
-      });
-
-      sendResult(result, reply.raw);
-    }
-  });
-
-  server.listen(
-    {port: 3000, host: "0.0.0.0"}, 
-    () => {
-    console.log(`Server is running on http://localhost:3000/`);
-  });
-}
+        if (shouldRenderGraphiQL(request)) {
+          reply.header("Content-Type", "text/html");
+          reply.send(
+            renderGraphiQL({
+              endpoint: "/graphql",
+            })
+          );
+  
+          return;
+        }
+  
+        const { operationName, query, variables } = getGraphQLParameters(request);
+  
+        const result = await processRequest({
+          request,
+          schema,
+          operationName,
+          query,
+          variables,
+        });
+  
+        sendResult(result, reply.raw);
+      }
+    });
+  
+    server.listen(
+      {port: 3000, host: "0.0.0.0"},
+      () => {
+      console.log(`GraphQL API is running on http://localhost:3000/graphql`);
+    });
+  }
 
 main();
